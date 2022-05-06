8c6e3df91ea00b84ae5086004e54592985eb04f1
omnisonic <omnisonic@gmail.com>
2022-05-06 14:25:24 -0700

    social icons configured

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,8 +26,11 @@ LINKS = (('Pelican', 'https://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ("github", "https://github.com/getpelican"),
+    ("rss", "/blog/feeds/all.atom.xml"),
+    ("twitter", "https://twitter.com/ThePSF")
+)
 
 DEFAULT_PAGINATION = 5
 
